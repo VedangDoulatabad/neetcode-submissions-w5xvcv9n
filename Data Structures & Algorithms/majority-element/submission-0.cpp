98c7c42ad160f9ced53028e354c962e5de1a9844
VedangDoulatabad <166922794+VedangDoulatabad@users.noreply.github.com>
2026-04-02 23:25:12 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> freq;
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        // Step 2: find majority
+        int n = nums.size();
+        for (auto it : freq) {
+            if (it.second > n / 2) {
+                return it.first;
+            }
+        }
+    }
+};
