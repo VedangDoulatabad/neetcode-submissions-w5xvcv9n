4ab52e25bb123d531ac4382b812976b13a51f7c3
VedangDoulatabad <166922794+VedangDoulatabad@users.noreply.github.com>
2026-04-09 23:17:14 +0530

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        unordered_set<int> s(nums.begin(), nums.end());
+        for(int i = 1 ; i<=nums.size() ; i++){
+            if(!s.count(i)){
+                return i;
+            }
+        }
+        return nums.size() + 1;
+    }
+};
