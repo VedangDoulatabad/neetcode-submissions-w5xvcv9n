711bfcc6709b5de609b990592afc4249fa689bb8
VedangDoulatabad <166922794+VedangDoulatabad@users.noreply.github.com>
2026-08-20 23:56:09 +0530

    Add: delete-leaves-with-a-given-value - submission-0

---
+++ Data Structures & Algorithms/delete-leaves-with-a-given-value/submission-0.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+
+        if (root == nullptr)
+            return nullptr;
+
+        // First process children
+        root->left = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
+
+        // Now check current node
+        if (root->left == nullptr &&
+            root->right == nullptr &&
+            root->val == target) {
+            return nullptr;
+        }
+
+        return root;
+    }
+};
+
+
+// Use Postorder traversal - It automatically processes child nodes before parent nodes
